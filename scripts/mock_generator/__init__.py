a142ba5b6c7fa502b67e122cd4c73a6ca78669a8
Maciej Gajdzica <maciej.gajdzica@solwit.com>
2017-09-20 16:03:15 +0200

    scripts: add generating mocks_global.h file.

--- scripts/mock_generator/__init__.py
+++ scripts/mock_generator/__init__.py
@@ -2,5 +2,6 @@ from mock_generator.fun_parse import fun_parse
 from mock_generator.fun_list_get import fun_list_get
 from mock_generator.generate_mock_h import generate_mock_h
 from mock_generator.generate_mock_c import generate_mock_c
+from mock_generator.generate_global_h import generate_global_h_base, global_h_insert
 
 __all__ = ["fun_list_get", "fun_parse", "generate_mock_h", "generate_mock_c"]
--- scripts/mocklib_generate.py
+++ scripts/mocklib_generate.py
@@ -1,7 +1,8 @@
 import argparse
 import os
 
-from mock_generator import *
+import mock_generator as mg
+#from mock_generator import *
 
 argp = argparse.ArgumentParser(description='Generate mocks from given header file.')
 argp.add_argument('file', help="Header file from which mock is generated.")
@@ -25,24 +26,43 @@ if inc_path[-1] is not '/':
     inc_path += '/'
 
 with open(filepath, "r") as file:
-    fun_list = fun_list_get(file.read())
+    fun_list = mg.fun_list_get(file.read())
 
 fun_data = []
 for fun in fun_list:
-    fun_name, ret_type, arg_types = fun_parse(fun)
+    fun_name, ret_type, arg_types = mg.fun_parse(fun)
     fun_data.append((fun_name, ret_type, arg_types))
-
-mock_h = generate_mock_h(file_no_ext, fun_data)
-mock_c = generate_mock_c(file_no_ext, inc_path, fun_data)
-
-mock_c_file = "{0}{1}/{1}.c".format(out_dir, file_no_ext)
-mock_h_file = "{0}{1}/{1}.h".format(out_dir, file_no_ext)
 
 if out_dir is not "" and not os.path.exists(out_dir + file_no_ext):
     os.makedirs(out_dir + file_no_ext)
 
+# Generate mock h file
+mock_h = mg.generate_mock_h(file_no_ext, fun_data)
+mock_h_file = "{0}{1}/{1}.h".format(out_dir, file_no_ext)
+
 with open(mock_h_file, "w") as file:
     file.write(mock_h)
 
+# Generate mock c file
+mock_c = mg.generate_mock_c(file_no_ext, inc_path, fun_data)
+mock_c_file = "{0}{1}/{1}.c".format(out_dir, file_no_ext)
+
 with open(mock_c_file, "w") as file:
     file.write(mock_c)
+
+# Generatee or update global file
+mock_global_file = "{0}mocks_global.h".format(out_dir)
+
+if os.path.isfile(mock_global_file):
+    file = open(mock_global_file, 'r')
+    mock_global_content = file.read()
+    file.close()
+else:
+    mock_global_content = mg.generate_global_h_base()
+
+with open(mock_global_file, 'w') as file:
+    try:
+        file.write(mg.global_h_insert(file_no_ext, fun_data, mock_global_content))
+    except ValueError:
+        print("Invalid mocks_global.h file - delete it.")
+
