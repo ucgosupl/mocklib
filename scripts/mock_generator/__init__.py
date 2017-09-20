aae39e1da48c13947b7496022d877b73805975af
Maciej Gajdzica <maciej.gajdzica@solwit.com>
2017-09-20 13:27:01 +0200

    scripts: fixed bug with mock_generator package imports.

--- scripts/mock_generator/__init__.py
+++ scripts/mock_generator/__init__.py
@@ -1,3 +1,6 @@
-from generate_mock_h import generate_mock_h
+from mock_generator.fun_parse import fun_parse
+from mock_generator.fun_list_get import fun_list_get
+from mock_generator.generate_mock_h import generate_mock_h
+from mock_generator.generate_mock_c import generate_mock_c
 
 __all__ = ["fun_list_get", "fun_parse", "generate_mock_h", "generate_mock_c"]
